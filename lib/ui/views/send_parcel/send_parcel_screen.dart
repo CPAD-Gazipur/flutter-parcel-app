2f4083deb6ab8d96b79fdc6affa6d5b2f7159ed1
alamin-karno <amin15-1951@diu.edu.bd>
2023-03-13 16:23:16 +0600

    Parcel App: Parcel Send Screen Added

---
+++ lib/ui/views/send_parcel/send_parcel_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SendParcelScreen extends StatelessWidget {
+  const SendParcelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        child: ListView(
+          children: [
+            Text(
+              'Send parcel',
+              style: Theme.of(context).textTheme.headline1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/views/views.dart
+++ lib/ui/views/views.dart
@@ -1,1 +1,4 @@
+export 'checkout/parcel_checkout_screen.dart';
+export 'details/parcel_details_screen.dart';
 export 'home/home_screen.dart';
+export 'send_parcel/send_parcel_screen.dart';
