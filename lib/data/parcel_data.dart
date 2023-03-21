d631dd289e063902d4cc2991ff7ace2970bc437b
alamin-karno <amin15-1951@diu.edu.bd>
2023-03-21 12:21:22 +0600

    Parcel App: Parcel Info Added into Parcel Size

--- lib/data/parcel_data.dart
+++ lib/data/parcel_data.dart
@@ -5,26 +5,26 @@ class ParcelData {
   static List<Parcel> parcelList = [
     Parcel(
       size: 'Small',
-      dimension: 'Max. 25 kg, 8 x 38 x 63 cm',
+      dimension: 'Max. 25 kg, 8 x 38 x 64 cm',
       description: 'Fits in envelop',
       image: ImageUtils.icParcelSizeSmall,
     ),
     Parcel(
       size: 'Medium',
-      dimension: 'Max. 35 kg, 8 x 38 x 63 cm',
-      description: 'Fits in envelop',
+      dimension: 'Max. 25 kg, 19 x 38 x 64 cm',
+      description: 'Fits in a shoe box',
       image: ImageUtils.icParcelSizeMedium,
     ),
     Parcel(
       size: 'Large',
-      dimension: 'Max. 46 kg, 8 x 38 x 63 cm',
-      description: 'Fits in envelop',
+      dimension: 'Max. 25 kg, 41 x 38 x 64 cm',
+      description: 'Fits in a cardboard box',
       image: ImageUtils.icParcelSizeLarge,
     ),
     Parcel(
       size: 'Custom',
-      dimension: 'Max. 100 kg, 8 x 38 x 63 cm',
-      description: 'Fits in envelop',
+      dimension: 'Max: 30kg or 300cm',
+      description: 'Fits on a skid',
       image: ImageUtils.icParcelSizeCustom,
     ),
   ];
